1bcd8b7bdea3a4364f513323079ce35ad20484b8
wk-j <somnuk.wk@gmail.com>
2018-06-02 21:19:39 +0700

    Await anything

---
+++ scripts/AwaitAnyting.csx
@@ -0,0 +1,7 @@
+#! "netcoreapp2.0"
+#r "../src/MagicMethod/obj/Debug/netcoreapp2.1/MagicMethod.dll"
+
+using MagicMethod;
+
+var myString = await 100;
+Console.WriteLine(myString);
---
+++ src/MagicMethod/AwaitAnything.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace MagicMethod {
+    public static class AwaiterExtensions {
+        public static Awaiter<int> GetAwaiter(this int val) {
+            return new Awaiter<int>(val);
+        }
+    }
+
+    public class Awaiter<T> : INotifyCompletion {
+        private string value;
+        public Awaiter(T val) {
+            value = val.ToString();
+        }
+
+        public bool IsCompleted { get { return true; } }
+        public void OnCompleted(Action func) {
+
+        }
+        public string GetResult() {
+            return value;
+        }
+    }
+}
